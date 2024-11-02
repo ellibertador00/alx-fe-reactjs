635a26eca3d500b023ed347e0e252dc266b160dd
fitsum <fitsumv7@gmail.com>
2024-11-02 12:59:55 +0300

    add header,footer,main jsx files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +13,10 @@ function App() {
   return (
     <>
       <div>
-        <WelcomeMessage/>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer/>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
