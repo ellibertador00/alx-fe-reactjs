2c6152fc843def60e77df85724363782c2a88420
fitsum <fitsumv7@gmail.com>
2024-11-02 20:14:39 +0300

    add

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,9 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx';
 import Header from './components/Header';
-import MainContent from './components/MainContent.jsx/index.js';
+import MainContent from './components/MainContent.jsx/';
 import Footer from './components/Footer.jsx';
-
+import UserProfile from './components/UserProfile.jsx';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,8 +16,8 @@ function App() {
         <WelcomeMessage />
         <Header />
         <MainContent />
-        
-        <Footer/>
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <Footer />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -38,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
 export default App
---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,10 @@
+function UserProfile(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.age}</p>
+      <p>{props.bio}</p>
+    </div>
+  );
+}
+export default UserProfile;
