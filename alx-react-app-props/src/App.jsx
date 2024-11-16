94f67682be6d709cb732012434b42489ff93a035
fitsum <fitsumv7@gmail.com>
2024-11-16 22:50:34 +0300

    addd

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,4 +1,4 @@
-import ProfilePage from "./ProfilePage";
+import ProfilePage from "./components/ProfilePage";
 import UserContext from "./components/UserContext";
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
