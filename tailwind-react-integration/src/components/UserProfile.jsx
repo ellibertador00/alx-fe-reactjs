b52b5b4a6a81f55ad18c62ae1a55142cdf86320e
fitsum <fitsumv7@gmail.com>
2024-12-02 23:52:40 +0300

    adddddd

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 shadow-lg rounded-lg mx-auto my-20   max-w-xs md:max-w-sm">
+    <div className="hover:shadow-xl bg-gray-100 sm:p-4 md:p-8 shadow-lg rounded-lg mx-auto my-20   max-w-xs md:max-w-sm">
       <img
-        className="rounded-full  mx-auto md:w-36 md:h-36 sm:w-24 sm:h-24"
+        className=" hover:scale-110   transition-transform duration-300 ease-in-out rounded-full  mx-auto md:w-36 md:h-36 sm:w-24 sm:h-24"
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className=" sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className=" hover:text-blue-500 sm:text-lg md:text-xl text-blue-800 my-4">
+        John Doe
+      </h1>
       <p className=" sm:text-sm text-gray-600 md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
